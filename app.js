b1f3bc7915abf5ce923c78c905f7e2d5372da7c8
SammitRane <rane.sammit@gmail.com>
2019-08-18 16:52:38 +0530

    Add and remove function for notes application

--- app.js
+++ app.js
@@ -1,62 +1,66 @@
-const chalk=require('chalk')
-const yargs=require('yargs')
-const getNotes = require('./notes.js')
-//Changing
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
 //Customize yargs version
-yargs.version('1.1.0')   
+yargs.version('1.1.0')
 
 
 //Create add Command
 yargs.command({
-    command:'add',
-    describe:'Adds a new note',
-    builder:{
-        title:{
-            describe:'Note title',
-            demandOption:true,
+    command: 'add',
+    describe: 'Adds a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
             type: 'string'
         },
-        body:{
-            describe:'Note body',
-            demandOption:true,
-            type:'string'
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
         }
     },
-    handler: function(argv){ 
-        console.log('Title: '+argv.title)
-        console.log('\nBody: '+argv.body)
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
     }
-        
+
 })
 
 //Create remove command
 yargs.command({
-    command:'remove',
-    describe:'Removes a note',
-    handler: function(){ 
-        console.log('Removing a note!')
+    command: 'remove',
+    describe: 'Removes a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title)
     }
-        
 })
 
 //Create Read command
 yargs.command({
-    command:'read',
-    describe:'Reads a note',
-    handler: function(){ 
+    command: 'read',
+    describe: 'Reads a note',
+    handler: function () {
         console.log('Reading a note!')
     }
-        
+
 })
 
 //Create list command
 yargs.command({
-    command:'list',
-    describe:'Lists all notes',
-    handler: function(){ 
+    command: 'list',
+    describe: 'Lists all notes',
+    handler: function () {
         console.log('Listing the notes!')
     }
-        
+
 })
 
 yargs.parse()
--- notes.js
+++ notes.js
@@ -1,4 +1,63 @@
-const getNotes = function(){
+const fs = require('fs')
+const chalk = require('chalk')
+const getNotes = function () {
     return 'Your notes.....'
 }
-module.exports=getNotes      
+
+const addNote = function (title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.bgGreen.white.inverse.bold('New note added!'))
+    }
+    else {
+        console.log(chalk.bgRed.white.inverse.bold('Note title taken!'))
+    }
+
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+
+}
+const loadNotes = function () {
+    try {
+
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+
+    } catch (e) {
+        return []
+    }
+}
+const removeNote = function (title) {
+    console.log('The note ' + title + ' will be removed')
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title
+    })
+    if (notes.length === notesToKeep.length) {
+        console.log(chalk.bgRed.white.inverse.bold('No note deleted!'))
+        saveNotes(notesToKeep)
+    }
+    else {
+        console.log(chalk.bgGreen.white.inverse.bold('Note deleted!'))
+    }
+
+
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+}
